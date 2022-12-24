ea8067ff11f200424f86bb48de32ba4735b230bc
VincentRPS <vincentbusiness55@gmail.com>
2022-12-24 22:29:10 +0800

    feat: add way to delete session

--- lib/sessions/session_registry.ex
+++ lib/sessions/session_registry.ex
@@ -18,6 +18,11 @@ defmodule Derailed.Session.Registry do
     GenServer.call(__MODULE__, {:new_session, ws_pid, user_id})
   end
 
+  @spec delete_session(String.t, String.t) :: nil
+  def delete_session(user_id, session_id) do
+    GenServer.call(__MODULE__, {:delete_session, user_id, session_id})
+  end
+
   # internal
   def handle_call({:get_sessions, user_id}, _from, state) do
     if Map.has_key?(state, user_id) == true do
@@ -29,7 +34,28 @@ defmodule Derailed.Session.Registry do
 
   def handle_call({:new_session, ws_pid, user_id}, _from, state) do
     {:ok, pid} = Derailed.Session.start(ws_pid, user_id)
+    sessions = Derailed.Session.Registry.get_sessions(user_id)
+    MapSet.put(sessions, pid)
+    Map.replace!(state, user_id, sessions)
     {:reply, pid, state}
+  end
+
+  def handle_call({:delete_session, user_id, session_pid}, _from, state) do
+    sessions = Derailed.Session.Registry.get_sessions(user_id)
+
+    for session <- sessions do
+      if session == session_pid do
+        MapSet.delete(sessions, session)
+      end
+    end
+
+    if MapSet.equal?(sessions, MapSet.new()) == false do
+      Map.replace!(state, user_id, sessions)
+    else
+      Map.delete(state, user_id)
+    end
+
+    {:noreply, user_id, state}
   end
 
   def init(_opts) do
